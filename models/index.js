09b2e38ced14e9b4067c5665d90d847bcee5e653
Ibrahim Yunus <younis.ib@gmail.com>
2019-11-16 14:29:17 -0500

    rechange name

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 // Exporting an object containing all of our models
-const Note = require("./banana.js/index.js");
-const Article = require("./mango.js/index.js");
+const Note = require("./Note.js/index.js.js");
+const Article = require("./Article.js/index.js.js");
 
 
 module.exports = {
